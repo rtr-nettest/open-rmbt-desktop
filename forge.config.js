ac4a91b2ac2df8d97149ec6b9071feaaa8978861
Polina Gurina <p.gurina@icloud.com>
2022-11-14 11:15:59 +0400

    Add config folders and icons

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,10 @@
+require("dotenv").config(process.env.RMBT_DESKTOP_DOTENV_CONFIG_PATH || ".env")
+const path = require("path")
+
 module.exports = {
-    packagerConfig: {},
+    packagerConfig: {
+        icon: path.join(process.env.CONFIG_FOLDER, "app-icon", "icon"),
+    },
     rebuildConfig: {},
     makers: [
         {

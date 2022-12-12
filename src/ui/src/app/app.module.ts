a9fc0639c1851f5da4b9d00dc103dc51c4ac8f45
Polina Gurina <p.gurina@icloud.com>
2022-12-12 14:02:11 +0400

    Adjust paths

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { HomeScreenComponent } from "./screens/home-screen/home-screen.component"
 import { TestScreenComponent } from "./screens/test-screen/test-screen.component"
-import { ResultScreenComponent } from "./screens/result-screen/result-screen.component";
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { StartTestButtonComponent } from './partials/start-test-button/start-test-button.component'
+import { ResultScreenComponent } from "./screens/result-screen/result-screen.component"
+import { HeaderComponent } from "./widgets/header/header.component"
+import { FooterComponent } from "./widgets/footer/footer.component"
+import { StartTestButtonComponent } from "./widgets/start-test-button/start-test-button.component"
 
 @NgModule({
     declarations: [

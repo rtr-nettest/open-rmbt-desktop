88398f582b0b62276cd935ea65abcc620f0bfd99
Polina Gurina <p.gurina@icloud.com>
2022-12-14 17:22:02 +0400

    Add getMeasurementResult to the library

---
+++ src/measurement/interfaces/simple-history-result.interface.ts
@@ -0,0 +1,9 @@
+export interface ISimpleHistoryResult {
+    measurementDate: string
+    measurementServerName: string
+    speedUpload: number
+    speedDownload: number
+    ping: number
+    providerName: string
+    ipAddress: string
+}

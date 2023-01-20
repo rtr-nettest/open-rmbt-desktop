7952a2774bef8dd1cab0b2d7c116c335b66fe1d2
Polina Gurina <p.gurina@icloud.com>
2023-01-20 12:33:24 +0400

    Experimental calculation

--- src/measurement/dto/measurement-thread-result-list.dto.ts
+++ src/measurement/dto/measurement-thread-result-list.dto.ts
@@ -15,7 +15,7 @@ export class MeasurementThreadResultList
 
     addResult(newBytes: number, newNsec: number) {
         Logger.I.info("New bytes: %d. New nsec: %d.", newBytes, newNsec)
-        // this.maxStoredResults / 7e9 ~= 10 results per 1e9 == 1 result per 1e8
+        const expectedNsecDiff = (20 * 7e9) / this.maxStoredResults
         let nsecDiff = newNsec
         if (this.resultsCounter > 0) {
             const prevNsec = this.nsec[this.resultsCounter - 1]
@@ -25,7 +25,7 @@ export class MeasurementThreadResultList
             newBytes >= 0 &&
             newNsec >= 0 &&
             this.resultsCounter < this.maxStoredResults &&
-            nsecDiff >= 1e8
+            nsecDiff >= expectedNsecDiff
         ) {
             this.bytes[this.resultsCounter] = newBytes
             this.nsec[this.resultsCounter] = newNsec

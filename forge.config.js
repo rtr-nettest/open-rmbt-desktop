e5f6cd25330802769a92c17ad5e2dc4a474df279
Dietmar Zlabinger <dietmar@zlabinger.at>
2023-03-01 09:20:43 +0100

    Add signing config

--- forge.config.js
+++ forge.config.js
@@ -19,6 +19,10 @@ module.exports = {
             name: "@electron-forge/maker-squirrel",
             config: {
                 name: "open-rmbt-desktop",
+                authors: "Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)",
+                description: "RTR Desktop app",
+                // https://www.files.certum.eu/documents/manual_en/Code-Signing-signing-the-code-using-tools-like-Singtool-and-Jarsigner_v2.3.pdf
+                signWithParams: "/fd sha256 /a /t http://time.certum.pl/"
             },
         },
         {

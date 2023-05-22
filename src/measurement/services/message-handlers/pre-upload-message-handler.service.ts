3f230f6898e5dfed4386338cdd7d737f5e03f728
Polina Gurina <p.gurina@icloud.com>
2023-05-22 21:52:23 +0400

    Revert "Rework pre-upload"
    This reverts commit cb25422bbaa5d0c1e3d01dbd35588b39e46424d4.

--- src/measurement/services/message-handlers/pre-upload-message-handler.service.ts
+++ src/measurement/services/message-handlers/pre-upload-message-handler.service.ts
@@ -11,10 +11,10 @@ import { ELoggerMessage } from "../../enums/logger-message.enum"
 
 export class PreUploadMessageHandler implements IMessageHandler {
     private chunkSize: number = RMBTClient.minChunkSize
-    private maxChunksCount = 2
+    private maxChunksCount = 16
+    private minChunkSize = 0
     private preUploadEndTime = Infinity
     private buffersMap: { [key: string]: Buffer[] } = {}
-    private chunkSizeReset = false
 
     constructor(
         private ctx: IMessageHandlerContext,
@@ -26,7 +26,7 @@ export class PreUploadMessageHandler implements IMessageHandler {
     }
 
     stopMessaging(): void {
-        this.onFinish?.(this.chunkSize * this.ctx.preUploadChunks)
+        this.onFinish?.(this.chunkSize)
     }
 
     writeData(): void {
@@ -66,25 +66,27 @@ export class PreUploadMessageHandler implements IMessageHandler {
             !this.ctx.preUploadChunks || this.ctx.preUploadChunks <= 0
                 ? 1
                 : this.ctx.preUploadChunks * 2
+        this.minChunkSize = this.ctx.preUploadChunks * this.chunkSize
         Logger.I.info(
             ELoggerMessage.T_WRITING_PUTNORESULT,
             this.ctx.index,
             ESocketMessage.PUTNORESULT,
-            this.chunkSize
+            this.chunkSize * this.ctx.preUploadChunks
         )
         this.ctx.client.write(
-            `${ESocketMessage.PUTNORESULT} ${this.chunkSize}\n`
+            `${ESocketMessage.PUTNORESULT} ${
+                this.chunkSize * this.ctx.preUploadChunks
+            }\n`
         )
     }
 
     private putNoResultIncreasingChunkSize() {
-        if (!this.chunkSizeReset) {
-            this.chunkSize = this.ctx.client.readableHighWaterMark
-            this.ctx.preUploadChunks = 1
-            this.chunkSizeReset = true
-        } else {
-            this.ctx.preUploadChunks = this.ctx.preUploadChunks * 2
-        }
+        this.chunkSize = Math.min(
+            RMBTClient.maxChunkSize,
+            Math.max(this.minChunkSize * 2, this.chunkSize * 2)
+        )
+        this.ctx.preUploadChunks = 1
+        this.maxChunksCount = 1
         Logger.I.info(
             ELoggerMessage.T_WRITING_PUTNORESULT,
             this.ctx.index,

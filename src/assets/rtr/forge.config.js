5acd49bb9ee262e6e7b0243ba0b697c8a6782c7b
Polina Gurina <p.gurina@icloud.com>
2023-07-25 12:22:21 +0400

    Add entitlements

--- src/assets/rtr/forge.config.js
+++ src/assets/rtr/forge.config.js
@@ -13,6 +13,7 @@ module.exports = {
         ],
         osxSign: {
             platform: "mas",
+            entitlements: path.resolve(__dirname, "entitlements.plist"),
         },
     },
     rebuildConfig: {},

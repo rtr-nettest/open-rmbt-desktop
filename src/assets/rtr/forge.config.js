7eda9c19afd867ec985c5ceb311fe497a28db489
Thomas Schreiber <thomas.schreiber@rtr.at>
2023-07-27 14:17:59 +0200

    update RTR manifest/forge config

--- src/assets/rtr/forge.config.js
+++ src/assets/rtr/forge.config.js
@@ -18,13 +18,17 @@ module.exports = {
             name: "@electron-forge/maker-appx",
             config: {
                 assets: path.resolve(__dirname, "app-icon"),
-                devCert:
-                    "C:\\Users\\%USERNAME%\\AppData\\Roaming\\electron-windows-store\\developmentca\\developmentca.pfx",
+//                devCert:
+  //                  "C:\\Users\\%USERNAME%\\AppData\\Roaming\\electron-windows-store\\developmentca\\developmentca.pfx",
                 manifest: path.resolve(__dirname, "AppXManifest.xml"),
                 packageDescription: "RTR Desktop App",
                 packageDisplayName: "RMBT Desktop",
+                authors: "Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)",
+                description: "RTR Desktop app",
+               // signWithParams: "/fd sha256 /a /t http://time.certum.pl/",
+               signtoolParams: ["/fd sha256", "/a", "/t http://time.certum.pl/"],
                 packageName: "RMBTDesktop",
-                publisher: "CN=developmentca",
+                publisher: "CN=Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)",
                 windowsKit:
                     "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.19041.0\\x64",
             },

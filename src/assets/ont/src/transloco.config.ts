28982de6cc17bdd83b940308f14f3ad3aafff48b
Polina Gurina <p.gurina@icloud.com>
2023-09-04 17:33:59 +0400

    Add more locales for ONT

--- src/assets/ont/src/transloco.config.ts
+++ src/assets/ont/src/transloco.config.ts
@@ -3,6 +3,12 @@ export const TranslocoConfigExt: { [key: string]: any } = {
     availableLocales: [
         { iso: "en", name: "English" },
         { iso: "de", name: "Deutsch" },
+        { iso: "nb", name: "Norsk" },
+        { iso: "sk", name: "Slovenčina" },
+        { iso: "sl", name: "Slovenščina" },
+        { iso: "sr", name: "Српски" },
+        { iso: "sr-Latn", name: "Srpski" },
+        { ison: "sr_ME-Latn", name: "Crnogorski" },
     ],
     defaultLang: "en",
 }

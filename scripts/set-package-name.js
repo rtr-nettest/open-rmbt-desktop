0a754a8e25358e290fc8b07aa3d51c9a8ece3ca2
Polina Gurina <p.gurina@icloud.com>
2023-10-19 15:37:32 +0400

    Add PACK variables

--- scripts/set-package-name.js
+++ scripts/set-package-name.js
@@ -3,15 +3,10 @@ const fs = require("fs")
 const pack = require("../package.json")
 
 function main() {
-    if (process.env.FLAVOR === "ont") {
-        pack.name = "open-nettest-desktop"
-        pack.productName = "Open Nettest"
-        pack.description = "Open Nettest Desktop App"
-    } else {
-        pack.name = "open-rmbt-desktop"
-        pack.productName = "RTR-Netztest"
-        pack.description = "RTR Desktop App"
-    }
+    pack.name = process.env.PACK_NAME
+    pack.productName = process.env.PACK_PRODUCT_NAME
+    pack.description = process.env.PACK_DESCRIPTION
+    pack.author = process.env.PACK_AUTHOR
     fs.writeFileSync("package.json", JSON.stringify(pack, null, 2))
 }
 

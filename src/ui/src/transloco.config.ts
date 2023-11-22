7a5fb45c0ca8ff4ac5a94a3acc818f730f3b246a
Dietmar Zlabinger <dietmar@zlabinger.at>
2023-11-22 17:29:16 +0100

    Add Spanish translation configuration, fix to es-ES

--- src/ui/src/transloco.config.ts
+++ src/ui/src/transloco.config.ts
@@ -1,9 +1,9 @@
 export const TranslocoConfigExt: { [key: string]: any } = {
-    availableLangs: ["en", "de", "es"],
+    availableLangs: ["en", "de", "es-ES"],
     availableLocales: [
         { iso: "en", name: "English" },
         { iso: "de", name: "Deutsch" },
-        { iso: "es", name: "Español" },
+        { iso: "es-ES", name: "Español" },
     ],
     browserLangs: ["en", "de"], // languages supported by the web portal
     defaultLang: "en",

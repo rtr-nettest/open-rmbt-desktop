1c8d3eb2a8cd0d19173edad4410317dd932df7cb
Dietmar Zlabinger <dietmar@zlabinger.at>
2023-11-23 12:37:03 +0100

    Add Slovenian language support

--- src/assets/rtr/src/transloco.config.ts
+++ src/assets/rtr/src/transloco.config.ts
@@ -1,9 +1,10 @@
 export const TranslocoConfigExt: { [key: string]: any } = {
-    availableLangs: ["en", "de", "es"],
+    availableLangs: ["en", "de", "es", "sl"],
     availableLocales: [
         { iso: "en", name: "English" },
         { iso: "de", name: "Deutsch" },
         { iso: "es", name: "Español" },
+        { iso: "sl", name: "Slovenščina" },
     ],
     browserLangs: ["en", "de"], // languages supported by the web portal
     defaultLang: "en",

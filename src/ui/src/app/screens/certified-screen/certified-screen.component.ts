877925108bf1a44c18fcdef0698f8851e46a28f5
Polina Gurina <p.gurina@icloud.com>
2023-12-06 12:37:47 +0100

    Add breadcrumbs

--- src/ui/src/app/screens/certified-screen/certified-screen.component.ts
+++ src/ui/src/app/screens/certified-screen/certified-screen.component.ts
@@ -7,7 +7,16 @@ import { MainStore } from "src/app/store/main.store"
     styleUrls: ["./certified-screen.component.scss"],
 })
 export class CertifiedScreenComponent {
+    activeBreadCrumbIndex = 0
+    breadCrumbs = ["Info", "Data", "Environment", "Measurement", "Result"]
     env$ = this.mainStore.env$
 
     constructor(private mainStore: MainStore) {}
+
+    nextBreadCrumb() {
+        if (this.activeBreadCrumbIndex >= this.breadCrumbs.length - 1) {
+            return
+        }
+        this.activeBreadCrumbIndex++
+    }
 }

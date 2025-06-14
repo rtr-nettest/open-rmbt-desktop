10313ff3033427e1e9f5ae0cd25b5567b1c9a099
Dietmar Zlabinger <dietmar@zlabinger.at>
2025-06-14 19:16:39 +0000

    Add AKOS assets

---
+++ src/assets/akos/src/app/enums/colors.enum.ts
@@ -0,0 +1,11 @@
+export enum EColors {
+  PRIMARY_100 = 'rgba(21, 56, 112, 1)',
+  PRIMARY_50 = 'rgba(21, 56, 112, 0.5)',
+  PRIMARY_25 = 'rgba(21, 56, 112, 0.25)',
+  PRIMARY_15 = 'rgba(21, 56, 112, 0.15)',
+  SECONDARY_100 = 'rgb(1,1,1,1.0)',
+  SECONDARY_50 = 'rgb(1,1,1,0.5)',
+  SECONDARY_10 = 'rgb(1,1,1,0.1)',
+  GRADIENT_100 = 'rgba(0,0,0,0.1)',
+  GRADIENT_0 = 'rgba(0,0,0,0.01)',
+}
---
+++ src/assets/akos/src/transloco.config.ts
@@ -0,0 +1,20 @@
+export const TranslocoConfigExt: { [key: string]: any } = {
+    availableLangs: ["en", "de", "es", "sl", "cs", "fr", "it", "no"],
+    crowdinMappings: { //for some languages, crowdin will default to lang/country
+        "es": "es-ES"
+    },
+    availableLocales: [
+        // languages shown in the Settings
+        { iso: "en", name: "English" },
+        { iso: "de", name: "Deutsch" },
+        { iso: "es", name: "Español" },
+        { iso: "sl", name: "Slovenščina" },
+        { iso: "cs", name: "Český" },
+        { iso: "fr", name: "Français" },
+        { iso: "it", name: "Italiano" },
+        { iso: "no", name: "Norsk" },
+    ],
+    browserLangs: ["en", "de", "sl"], // languages supported by the web portal
+    defaultLang: "en",
+    defaultBrowserLang: "en",
+}

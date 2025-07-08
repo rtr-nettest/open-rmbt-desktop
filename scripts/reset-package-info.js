5c816549aacd317c22a580756bd8d05fd55d5739
Dietmar Zlabinger <dietmar@zlabinger.at>
2025-07-08 08:56:08 +0200

    Fix build issue

--- scripts/reset-package-info.js
+++ scripts/reset-package-info.js
@@ -1,4 +1,7 @@
-require("dotenv").config(process.env.RMBT_DESKTOP_DOTENV_CONFIG_PATH || ".env")
+require("dotenv").config({
+    path: process.env.RMBT_DESKTOP_DOTENV_CONFIG_PATH || ".env"
+})
+
 const fs = require("fs")
 const pack = require("../package.json")
 

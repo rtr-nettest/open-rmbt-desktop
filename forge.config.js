c86f85eeedd8ca379027b5c0b502d4625e7a6fd6
Dietmar Zlabinger <dietmar@zlabinger.at>
2025-07-08 08:56:08 +0200

    Fix build issue

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,7 @@
-require("dotenv").config(process.env.RMBT_DESKTOP_DOTENV_CONFIG_PATH || ".env")
+require("dotenv").config({
+    path: process.env.RMBT_DESKTOP_DOTENV_CONFIG_PATH || ".env"
+})
+
 const path = require("path")
 const customerConfig = require(path.resolve(
     process.env.ASSETS_FOLDER,
